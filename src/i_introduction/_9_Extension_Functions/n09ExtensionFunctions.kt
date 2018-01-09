25507ad7a64a8baec7d5ede0ebbc2b67a341a9e5
Angelo Sciarra <asciarra@byte-code.com>
2018-01-09 19:02:33 +0100

    TASK9 minor changes

--- src/i_introduction/_9_Extension_Functions/n09ExtensionFunctions.kt
+++ src/i_introduction/_9_Extension_Functions/n09ExtensionFunctions.kt
@@ -6,15 +6,6 @@ import util.doc9
 // declares an extension function that returns the last character
 fun String.lastChar() = this.get(this.length - 1)
 
-
-// 'this' refers to the receiver (String) and can be omitted
-fun String.lastChar1() = get(length - 1)
-
-fun useExtensionFunction() {
-    // try Ctrl+Space "default completion" after the dot: lastChar() is visible
-    "abc".lastChar()
-}
-
 // 'lastChar' is compiled to a static function in the class ExtensionFunctionsKt (see JavaCode9.useExtension)
 
 fun todoTask9(): Nothing = TODO(
@@ -30,6 +21,6 @@ fun todoTask9(): Nothing = TODO(
 data class RationalNumber(val numerator: Int, val denominator: Int)
 
 fun Int.r(): RationalNumber = RationalNumber(this, 1)
-fun Pair<Int, Int>.r(): RationalNumber = RationalNumber(this.first, this.second)
+fun Pair<Int, Int>.r(): RationalNumber = RationalNumber(first, second)
 
 
