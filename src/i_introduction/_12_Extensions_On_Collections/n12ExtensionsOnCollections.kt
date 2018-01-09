1c91b99b17d64d4b52481e9fe362b8291ce5f41e
Angelo Sciarra <asciarra@byte-code.com>
2018-01-09 22:13:57 +0100

    Task12 Completed

--- src/i_introduction/_12_Extensions_On_Collections/n12ExtensionsOnCollections.kt
+++ src/i_introduction/_12_Extensions_On_Collections/n12ExtensionsOnCollections.kt
@@ -17,7 +17,7 @@ fun todoTask12(): Nothing = TODO(
 )
 
 fun task12(): List<Int> {
-    todoTask12()
-    return arrayListOf(1, 5, 2)
+    val list = arrayListOf(1, 5, 2)
+    return list.sortedDescending()
 }
 
