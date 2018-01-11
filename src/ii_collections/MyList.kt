5a71cb535b1e8edd8a6b3115b39b139f3abbf5d3
eureka84 <asciarra@byte-code.com>
2018-01-11 23:49:16 +0100

    minor change

--- src/ii_collections/MyList.kt
+++ src/ii_collections/MyList.kt
@@ -7,7 +7,7 @@ interface MyList<T> {
     fun head(): T
     fun tail(): MyList<T>
     fun prepend(elem: T): MyList<T>
-    fun <R> foldLeft(initial: R, operation: (acc: R, elem: T) -> R): R
+    fun <R> foldLeft(initial: R, combine: (acc: R, elem: T) -> R): R
 }
 
 object Nil: MyList<Any> {
@@ -16,14 +16,14 @@ object Nil: MyList<Any> {
     }
     override fun tail(): MyList<Any> = this
     override fun prepend(elem: Any): MyList<Any> = Cons(elem, Nil)
-    override fun <R> foldLeft(initial: R, operation: (acc: R, elem: Any) -> R): R = initial
+    override fun <R> foldLeft(initial: R, combine: (acc: R, elem: Any) -> R): R = initial
 }
 
 data class Cons<T>(private val head: T, private val tail: MyList<T>): MyList<T> {
     override fun head(): T = head
     override fun tail(): MyList<T> = tail
     override fun prepend(elem: T): MyList<T> = Cons(elem, this)
-    override fun <R> foldLeft(initial: R, operation: (acc: R, elem: T) -> R): R {
+    override fun <R> foldLeft(initial: R, combine: (acc: R, elem: T) -> R): R {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
