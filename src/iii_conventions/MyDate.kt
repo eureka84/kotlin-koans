d59410735a7b9c370b3ae6675d117d5399d3d906
eureka84 <angelosciarra@ymail.com>
2018-01-16 15:06:11 +0100

    Task29 Completed

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,24 +1,34 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int): Comparable<MyDate> {
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
     override fun compareTo(other: MyDate): Int = when {
         year - other.year != 0 -> year - other.year
         month - other.month != 0 -> month - other.month
         else -> dayOfMonth - other.dayOfMonth
     }
+
+    operator fun plus(ri: RepeatedTimeInterval): MyDate = this.addTimeIntervals(ri.timeInterval, ri.times)
 }
 
 operator fun MyDate.rangeTo(other: MyDate): DateRange = DateRange(this, other)
 
-enum class TimeInterval {
-    DAY,
-    WEEK,
-    YEAR
+enum class TimeInterval(private val equivalentNumberOfDays: Int) {
+    DAY(1),
+    WEEK(7),
+    YEAR(365);
+
+    operator fun plus(that: TimeInterval): RepeatedTimeInterval =
+            RepeatedTimeInterval(DAY, this.equivalentNumberOfDays + that.equivalentNumberOfDays)
+
+    operator fun times(i: Int): RepeatedTimeInterval = RepeatedTimeInterval(this, i)
+
 }
+
+class RepeatedTimeInterval(val timeInterval: TimeInterval, val times: Int)
 
 class DateRange(
         override val start: MyDate,
-        override val endInclusive: MyDate): ClosedRange<MyDate>, Iterable<MyDate> {
+        override val endInclusive: MyDate) : ClosedRange<MyDate>, Iterable<MyDate> {
 
     override fun iterator(): Iterator<MyDate> {
 
