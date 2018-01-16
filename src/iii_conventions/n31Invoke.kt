0d8830a4ac25a7338d658a8f9683784d3cd89f17
eureka84 <angelosciarra@ymail.com>
2018-01-16 21:16:57 +0100

    Task31 Completed

--- src/iii_conventions/n31Invoke.kt
+++ src/iii_conventions/n31Invoke.kt
@@ -3,17 +3,19 @@ package iii_conventions
 import util.TODO
 
 
-class Invokable
+class Invokable(private val numberOfInvocations: Int = 0) {
+    operator fun invoke(): Invokable = Invokable(numberOfInvocations + 1)
+    fun getNumberOfInvocations(): Int = numberOfInvocations
+}
 
 fun todoTask31(): Nothing = TODO(
-    """
+        """
         Task 31.
         Change the class 'Invokable' to count the number of invocations:
         for 'invokable()()()()' it should be 4.
     """,
-    references = { invokable: Invokable -> })
+        references = { invokable: Invokable -> })
 
 fun task31(invokable: Invokable): Int {
-    todoTask31()
-//    return invokable()()()().getNumberOfInvocations()
+    return invokable()()()().getNumberOfInvocations()
 }
